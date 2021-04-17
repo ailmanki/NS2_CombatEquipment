e5bc5547e990127dc84c74bd1b303b8d1f957fa4
diego <1570625+ailmanki@users.noreply.github.com>
2021-04-17 13:12:58 +0200

    Only require armor 1 to get sentries.

--- source/lua/NS2_CombatEquipment/Combat/ExperienceData.lua
+++ source/lua/NS2_CombatEquipment/Combat/ExperienceData.lua
@@ -2,6 +2,6 @@ local upgrade = CombatMarineUpgrade()
 
 --team, upgradeId, upgradeTextCode, upgradeDescription, upgradeTechId, upgradeFunc, requirements, levels, upgradeType, refundUpgrade, hardCap, mutuallyExclusive, needsNearComm)
 local points = 1
-upgrade:Initialize(kCombatUpgrades.Sentries, "sentries", "Sentry", kTechId.DropSentry, nil, kCombatUpgrades.Armor2, points, kCombatUpgradeTypes.Weapon, false, 0, nil)
+upgrade:Initialize(kCombatUpgrades.Sentries, "sentries", "Sentry", kTechId.DropSentry, nil, kCombatUpgrades.Armor1, points, kCombatUpgradeTypes.Weapon, false, 0, nil)
 
 table.insert(UpsList, upgrade)
