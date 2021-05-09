115ef9ce33a663fd45fd8449451ae9941d7a7f48
diego <1570625+ailmanki@users.noreply.github.com>
2021-05-09 17:10:57 +0200

    Update from Shrimm! - Limit sentries to 1/3 per team - removed requirement for armor 1 - increased cost to 2

--- source/lua/NS2_CombatEquipment/Combat/ExperienceData.lua
+++ source/lua/NS2_CombatEquipment/Combat/ExperienceData.lua
@@ -1,7 +1,7 @@
 local upgrade = CombatMarineUpgrade()
 
 --team, upgradeId, upgradeTextCode, upgradeDescription, upgradeTechId, upgradeFunc, requirements, levels, upgradeType, refundUpgrade, hardCap, mutuallyExclusive, needsNearComm)
-local points = 1
-upgrade:Initialize(kCombatUpgrades.Sentries, "sentries", "Sentry", kTechId.DropSentry, nil, kCombatUpgrades.Armor1, points, kCombatUpgradeTypes.Weapon, false, 0, nil)
+local points = 2
+upgrade:Initialize(kCombatUpgrades.Sentries, "sentries", "Sentry", kTechId.DropSentry, nil, kCombatUpgrades.Welder, points, kCombatUpgradeTypes.Weapon, false, 1/3,  nil)
 
 table.insert(UpsList, upgrade)
